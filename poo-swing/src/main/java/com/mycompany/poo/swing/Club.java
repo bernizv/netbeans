0a47ead4a369ff6d7caa6e0d311fecf596dab030
bernizv <vrberniz@gmail.com>
2019-10-06 13:04:46 -0300

    poo swing

---
+++ poo-swing/src/main/java/com/mycompany/poo/swing/Club.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.poo.swing;
+
+/**
+ *
+ * @author NOLE201903
+ */
+public class Club {
+    Integer rvitorias = 0;
+    Integer rderrrotas = 0;
+    Integer rempates = 0;
+    Integer pontuacao = 0;
+    
+    void rvitorias(){
+    rvitorias ++;
+    pontuacao += 3;
+    }
+    
+    void rderrotas(){
+    
+    }
+}
